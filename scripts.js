d269f554449dbc5a8d716221994f80eb0df86c7f
Kevin Liu <kevinliu314@gmail.com>
2016-03-27 15:00:44 -0400

    simple functionality set up
    perhaps implement styles later

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 var channels=["freecodecamp","EternaLEnVyy","Arteezy","ohhhmyyy","RaineAndShine","DotACapitalist",
 "drsushi11","petey_did_it","Slaye","KeyboardHead_","Tiberoa","DOTAgasm","MotPax","koreyah","dota2ti","ZyoriTV",
-"FLUFFDota","fwoshy","eloise_ailv","ByeondTheSummit"]
+"FLUFFDota","fwoshy","eloise_ailv","BeyondTheSummit","brunofin"]
 
 function getChannelData(){
   channels.forEach(function(item){
@@ -23,17 +23,19 @@ function getChannelData(){
         status="Online"
       }
       $.getJSON(makeURL("channels",item),function(data){
-        var logo = data.logo != null ? data.logo : "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
-        var name=data.display_name;
-        
+        var logo = data.logo != null ? data.logo : "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+        var name = data.display_name!= null ? data.display_name : item;
+        var html = "<div class='row "+status+"'><div class='col-xs-2 col-sm-1' id='icon'>"+
+        "<img src='"+logo+"' class='logo'></div><div class='col-xs-10 col-sm-3' id='name'><a href='"+
+        data.url+"' target='_blank'>"+name+"</a></div><div class='col-xs-10 col-sm-8' id='streaming'>"+
+        game+"</div></div>";
+        status === "Online" ? $("#stream_container").prepend(html) : $("#stream_container").append(html);
       })
     });
   })
 }
 
 $(document).ready(function(){
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-    console.log(data);
-  });
+  getChannelData();
 
 });
